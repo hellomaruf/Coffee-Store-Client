05d2c9bc70ccfea6281721078fba12a897d64da8
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-22 11:55:10 +0600

    Added add new coffee page

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,48 +1,117 @@
-
-
 function AddCoffee() {
   return (
     <div>
-      <section className="p-6 dark:bg-gray-100 dark:text-gray-900">
-	<form noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
-		<fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-50">
-		
-			<div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="firstname" className="text-sm">First name</label>
-					<input id="firstname" type="text" placeholder="First name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="lastname" className="text-sm">Last name</label>
-					<input id="lastname" type="text" placeholder="Last name" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-3">
-					<label htmlFor="email" className="text-sm">Email</label>
-					<input id="email" type="email" placeholder="Email" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full">
-					<label htmlFor="address" className="text-sm">Address</label>
-					<input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="city" className="text-sm">City</label>
-					<input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="state" className="text-sm">State / Province</label>
-					<input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-				<div className="col-span-full sm:col-span-2">
-					<label htmlFor="zip" className="text-sm">ZIP / Postal</label>
-					<input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ring-opacity-75 dark:text-gray-50 focus:dark:ring-violet-600 dark:border-gray-300" />
-				</div>
-			</div>
-		</fieldset>
-	
-	</form>
-</section>
+      <section className="p-6 max-w-7xl mx-auto dark:text-gray-900">
+        <div className=" text-center pb-10">
+          <h1 className="font-bold text-3xl pb-3">Add New Coffee</h1>
+          <p className="max-w-2xl mx-auto">
+            It is a long established fact that a reader will be distraceted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using Content here.
+          </p>
+        </div>
+        <form
+          noValidate=""
+          action=""
+          className="container flex flex-col mx-auto space-y-12"
+        >
+          <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm bg-[#F4F3F0]">
+            <div className="grid grid-cols-6 gap-4 col-span-full ">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-lg font-medium">
+                  Name
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="Enter Coffee name"
+                  className="w-full rounded-md  p-2 outline-none border"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-lg font-medium">
+                  Chef
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Enter Chef name"
+                  className="w-full rounded-md p-2 outline-none border "
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full ">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-lg font-medium">
+                  Supplier
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="Enter Coffee Supplier"
+                  className="w-full rounded-md  p-2 outline-none border"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-lg font-medium">
+                  Teste
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Enter Coffee Teste"
+                  className="w-full rounded-md p-2 outline-none border "
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full ">
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="firstname" className="text-lg font-medium">
+                  Category
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="Enter Coffee Category"
+                  className="w-full rounded-md  p-2 outline-none border"
+                />
+              </div>
+              <div className="col-span-full sm:col-span-3">
+                <label htmlFor="lastname" className="text-lg font-medium">
+                  Details
+                </label>
+                <input
+                  id="lastname"
+                  type="text"
+                  placeholder="Enter Coffee Details"
+                  className="w-full rounded-md p-2 outline-none border "
+                />
+              </div>
+            </div>
+            <div className="grid grid-cols-6 gap-4 col-span-full ">
+              <div className="col-span-full ">
+                <label htmlFor="firstname" className="text-lg font-medium">
+                  Photo
+                </label>
+                <input
+                  id="firstname"
+                  type="text"
+                  placeholder="Enter Photo URL"
+                  className="w-full rounded-md  p-2 outline-none border"
+                />
+                <div className=" w-full">
+                  <button className="bg-[#D2B48C] w-full mt-8 py-2 font-bold text-gray-700 rounded-lg">
+                    Add Coffee
+                  </button>
+                </div>
+              </div>
+            </div>
+          </fieldset>
+        </form>
+      </section>
     </div>
-  )
+  );
 }
 
-export default AddCoffee
+export default AddCoffee;
