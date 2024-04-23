344250f1a49c97b7361f63be56c9a712bae670b0
maruf-ahmed <marufahamed1110@gmail.com>
2024-04-23 20:06:37 +0600

    Added delete functionality

--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 function User() {
   const loadedUser = useLoaderData();
+  const [users, setUsers] = useState(loadedUser);
+  const handleDelete = (id) => {
+    console.log(id);
+    fetch(`http://localhost:3000/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount === 1) {
+          alert("Deleted Successfully");
+          const remaining = users.filter((user) => user._id !== id);
+          setUsers(remaining);
+        }
+      });
+  };
   return (
     <div className="max-w-7xl mx-auto">
       <h2 className="text-2xl font-semibold text-center py-5"> All users </h2>
@@ -18,12 +34,18 @@ function User() {
           </thead>
           <tbody>
             {/* row 1 */}
-            {loadedUser.map((user, index) => (
+            {users.map((user, index) => (
               <tr key={index}>
                 <th>1</th>
                 <td>{user.email}</td>
                 <td>Quality Control Specialist</td>
                 <td>Blue</td>
+                <td>
+                  <button onClick={() => handleDelete(user._id)}>
+                    {" "}
+                    <i className="ri-delete-bin-5-line bg-[#EA4744] text-white p-2 rounded-md text-xl mx-4"></i>
+                  </button>
+                </td>
               </tr>
             ))}
           </tbody>
